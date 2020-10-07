d6a39d50ccaa8540128511bd6eab5c9d424ef345
khawla ilahi <khawla.ilahi1@gmail.com>
2020-10-07 14:01:53 +0400

    displayChallenges

--- project-Files/main.js
+++ project-Files/main.js
@@ -29,10 +29,47 @@ $(document).ready(function() {
 							   {challenge:"meditate for 5 minutes and focus only on your breathing", pic:"images/meditate.jpg"},{challenge:"Dance or sing outloud in public", pic:"images/dance.gif"},
 							   {challenge:"Forgive someone who wronged you", pic:"images/forgive.jpg"}];
 	
-       var socialChallenges = ["Plant a tree/plant", "Buy a meal for a homeless person", "Tell a family member that you love him/her and describe why", "Pick up any rubbish you find on the street and put it a garbage-can",
-   							"Be an good listner" , "ask someone about their problems and actively listen to them", "Make a donation wether with clothes/money/time .."
 
-                           ];
+       var socialChallenges =[{challenge:"Plant a tree/plant", pic:"images/plant.jpg"},{challenge:"Buy a meal for a homeless person", pic:"images/homeless.jpg"},
+							   {challenge:"Pick up any rubbish you find on the street and put it a garbage-can", pic:"images/trash.jpeg"},{challenge:"Make a donation wether with clothes/money/time ..", pic:"images/donation.jpg"},
+							   {challenge:"Be an good listner ask someone about their problems and actively listen to them", pic:"images/listner.jpg"},{challenge:"Tell a family member that you love him/her and describe why", pic:"images/love.jpg"}
+							   ];
+var i =0;
+var j=0;
 
+	function persoChallenge(i){
 
-})
+		$("#chal").text(persoChallenges[i].challenge);
+		$("#chal").append("<br><br><img class='pic' src='"+persoChallenges[i].pic+"'>");
+	}
+
+	function socialChallenge(j){
+		$("#chal").text(socialChallenges[j].challenge);
+		$("#chal").append("<br><br><img class='pic' src='"+socialChallenges[j].pic+"'>");
+	}
+
+    $("#perso").click(function(){
+    	if(i >= persoChallenges.length)
+			i=0;
+    	$(".choices").css("display","none");
+    	$(".challenge").css("display","block");
+    	persoChallenge(i);
+    	i++;
+    	
+    });
+
+    $("#social").click(function(){
+    	if(j >= socialChallenges.length)
+    		j=0;
+    	$(".choices").css("display","none");
+    	$(".challenge").css("display","block");
+    	socialChallenge(j);
+    	j++;
+    	
+    });
+
+  $("#decline").click(function(){
+  	$(".choices").css("display","block");
+    $(".challenge").css("display","none");
+     });
+});
