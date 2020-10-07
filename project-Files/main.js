eb8195d8ca69d9efce81739e6e6cb4580b18cf66
khawla ilahi <khawla.ilahi1@gmail.com>
2020-10-08 00:32:36 +0400

    signUpAndChallFunctions+acceptedDiv

--- project-Files/main.js
+++ project-Files/main.js
@@ -9,7 +9,10 @@ function each (coll, func){
           }
     }
  }
+ //sign-out button will reload the page because i dont know another way to signout 
+$("#sign-out").click(function(){location.reload();})
 
+//this is the sencond page 
 $(".container").hide()
 
 	// this is for the sign/ login (container div)
@@ -42,39 +45,64 @@ $(".container").hide()
 							   {challenge:"Pick up any rubbish you find on the street and put it a garbage-can", pic:"images/trash.jpeg"},{challenge:"Make a donation wether with clothes/money/time ..", pic:"images/donation.jpg"},
 							   {challenge:"Be an good listner ask someone about their problems and actively listen to them", pic:"images/listner.jpg"},{challenge:"Tell a family member that you love him/her and describe why", pic:"images/love.jpg"}
 							   ];
-		var i =0;
-		var j=0;
+		var i ;
+		var j;
+		var profileIndex;
 		//array of all the profile users 
-		let profiles=[];
+		var profiles=[];
+		activeUserPerso()
+		function activeUserPerso(){
+			for(var k = 0 ; k < profiles.length ; k++){
+				 if((profiles[k].info).username === activeUser){
+					 i =  profiles[k].persoIndex ; 
+					 j =  profiles[k].socialIndex ; 
+					 profileIndex = k;
+				}
+			}
+			console.log(i);
+			console.log(j);
 
-	function persoChallenge(i){
+		}
+	 
+	function persoChallenge(){
 
+		console.log(i);
+		if(i >= persoChallenges.length)
+			i=0;
 		$("#chal").text(persoChallenges[i].challenge);
 		$("#chal").append("<br><br><img class='pic' src='"+persoChallenges[i].pic+"'>");
+		
 	}
 
-	function socialChallenge(j){
+	function socialChallenge(){
+		 
+		if(j >= socialChallenges.length)
+    		j=0;
 		$("#chal").text(socialChallenges[j].challenge);
 		$("#chal").append("<br><br><img class='pic' src='"+socialChallenges[j].pic+"'>");
+		
 	}
 
     $("#perso").click(function(){
-    	if(i >= persoChallenges.length)
-			i=0;
+   //  	if(i >= persoChallenges.length)
+			// i=0;
+			console.log(i)
     	$(".choices").css("display","none");
     	$(".challenge").css("display","block");
-    	persoChallenge(i);
-    	i++;
+    	persoChallenge();
+        socialOrPerso= 2;
     	console.log(profiles)
-    	
+    	i++;
     });
-
+    //this varible will help me know if he accepts a social or personal challenge : if 1 it's perso, if 2 it's social
+var socialOrPerso = 0;
     $("#social").click(function(){
-    	if(j >= socialChallenges.length)
-    		j=0;
+    	// if(j >= socialChallenges.length)
+    	// 	j=0;
     	$(".choices").css("display","none");
     	$(".challenge").css("display","block");
-    	socialChallenge(j);
+    	socialChallenge();
+    	socialOrPerso= 2;
     	j++;
     	
     });
@@ -82,7 +110,30 @@ $(".container").hide()
   $("#decline").click(function(){
   	$(".choices").css("display","block");
     $(".challenge").css("display","none");
+    socialOrPerso = 0;
      });
+
+$("#accept").click(function(){
+	if(socialOrPerso === 1){
+		profiles[profileIndex].persoIndex +=1;
+		profiles[profileIndex].persoAccepted +=1;
+	}if(socialOrPerso === 2){
+		profiles[profileIndex].socialIndex +=1;
+	    profiles[profileIndex].socialAccepted +=1;
+	 }   
+	$("#day").text("Have An Amazing Day " + activeUser + " !");
+	$("#accepted").css("display", "block");
+	$(".challenge").css("display","none");
+})
+
+
+$("#home").click(function(){
+$("#accepted").css("display", "none");
+$(".choices").css("display","block");
+})
+
+  //this function check is there is another user with same email or username
+  //returns false is there's not 
  function UniqUser(username, email){
     var result = false;
  	each(profiles,function(element, i){
@@ -95,6 +146,15 @@ $(".container").hide()
  	});
     return result;
 }
+//to the next page
+ function GoToPage2(){
+ 	    $(".container").css("display","none");
+    	$(".secondPage").css("display","block");
+    	$("#sign-out").css("display","block");
+    	$("#sign-in").css("display","none");
+ }
+var activeUser;
+//checks the validity if the user's input at registration 
 function ValidReg(){
 	var username= $("#username").val();
 	var email= $("#emailregister").val();
@@ -117,11 +177,15 @@ function ValidReg(){
         
 		}else{
 		factory(username, email , password);
-		$(".container").css("display","none");
-    	$(".secondPage").css("display","block");
+		GoToPage2();
+		activeUser = username;
+		console.log(activeUser);
+        activeUserPerso();
+	 	
     }
 
 }
+
 //factory function for new users
 function factory(username, email , password){
 	
@@ -138,8 +202,26 @@ function factory(username, email , password){
 	 $("#passwordregister").val("");
 }
 
+//this function checks if the users email and password correspond to an account or not  
+function login(){
+	var exist=false;
+	var email= $("#emaillogin").val();
+	var password= $("#passwordlogin").val();
+	each(profiles, function(element, i){
+ 			if(element.info.email === email && element.info.password === password)
+ 				exist= true;
+            
+	})
 
-
+	 if(exist){
+	 	GoToPage2();
+	 	activeUser=username;
+	 	console.log(activeUser)
+	 	activeUserPerso()
+	 	}else{
+     	alert("The email you’ve entered doesn’t match any account. please register first")
+     }
+}
 
 
 
